126f4861d0b9ef68c1727b162fb14b8ad6c5bfc2
J V <jana.vitvarova@tul.cz>
2025-10-23 13:05:52 +0200

    Adjust ending condition, while found stop

--- src/week06_for_loop/GuessingNumber.java
+++ src/week06_for_loop/GuessingNumber.java
@@ -52,7 +52,7 @@ public class GuessingNumber {
         int min = lower;
         int max = upper;
         
-        while(min <= max){//program ma skoncit, kdyz uhodl nebo kdyz min > max 
+        while(!found && min <= max){//program ma skoncit, kdyz uhodl nebo kdyz min > max 
             middle = (max + min)/2;
             System.out.println("Myslel jsi si cislo " + middle + "?");
             menuAnswers();
